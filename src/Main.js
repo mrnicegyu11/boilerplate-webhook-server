85c9e22c38e630acfcbcfe33218f30369b5626aa
Dustin Kaiser <dustin.kaiser@gmx.net>
2021-06-15 15:21:28 +0200

    First functionality

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const config = require('./config');
+//const config = require('./config');
+const dimoco = require("@mrnicegyu11/dimocopayment");
 
 // Body Parsers
 app.use(express.urlencoded({ extended: false }));
@@ -11,15 +12,17 @@ app.get('/', (req, res) => res.status(200).json({ error: false, message: 'Hi the
 
 // Handle the web hook
 app.post('/webhook', (req, res) => {
-    if (req.headers['Authorization'] != config.authorization) return res.status(401).json({ error: true, messgae: 'Icorrect authorization token' });
+    //if (req.headers['Authorization'] != config.authorization) return res.status(401).json({ error: true, messgae: 'Icorrect authorization token' });
     console.log(req.body);
+    const webhookData = dimoco.processWebhook(req.body,"KPaR2jeR");
+    console.log(webhookData);
     res.status(200).json({ error: true, message: 'Success' });
 });
 
 // Route not found
 app.use('*', (req, res) => res.status(400).json({ error: true, message: 'The requested endpoint could not be found' }));
-
+const webPort = 42004;
 // Listen the server
-app.listen(config.port, () => {
-    console.log('Server listening on port: ' + config.port);
+app.listen(webPort, () => {
+    console.log('Server listening on port: ' + webPort.toString());
 });
